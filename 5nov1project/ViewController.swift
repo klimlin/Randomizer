5b2eb9c1b10db5f854d5fdbe5c0caa51c1bfca1e
Alina <linalevel5@gmail.com>
2022-11-05 13:44:40 +0300

    Initial Commit

---
+++ 5nov1project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5nov1project
+//
+//  Created by MAcbook on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
